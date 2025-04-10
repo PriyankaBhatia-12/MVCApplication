ba5aa97d8b0bf2bc6fcb91fe08358c44b569b318
PriyankaBhatia-12 <priyankasherlybhatia@gmail.com>
2025-04-10 14:25:11 +0530

    Initial commit

---
+++ MVCApplication/Controllers/HelloController1.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+
+
+namespace MvcCoreLab1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Student()
+        {
+            TempData["Location"] = "India";
+
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Priyanka",
+                Gender = "Female"
+
+            };
+            ViewBag.CurrentDate = DateTime.Now.ToShortDateString();
+            ViewData["Title"] = "Dr.";
+            string? location = Convert.ToString(TempData["Location"]);
+            return View(student);
+
+
+        }
+    }
+}
+
+
